31f5f6c903fd3798f47b9592821529e1393793db
davidsebesta1 <david.sebesta@post.cz>
2023-03-12 11:38:27 +0100

    Add project files.

---
+++ TestPlugin/Config.cs
@@ -0,0 +1,16 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Core.Factories;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using System;
+
+namespace TestPlugin.Plugin
+{
+
+    internal class Config
+    {
+
+
+    }
+}
---
+++ TestPlugin/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace TestPlugin.Plugin
+{
+    internal class Plugin
+    {
+        [PluginConfig]
+        public Config Configuration;
+
+        [PluginEntryPoint("Testing Plugin", "1.0.0", "testing testing", "davidsebesta")]
+        void Enabled() => EventManager.RegisterEvents((object)this);
+
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        void OnPlayerJoin(Player player)
+        {
+            Log.Info($"Player &6{player.UserId}&r joined this server, logged via plugin.");
+        }
+    }
+}
